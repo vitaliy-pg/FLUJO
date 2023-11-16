eb46d59e424b37ff7eb54bc3198017d3e9cf290a
vitaliy-pg <aljndrvt@myuax.com>
2023-11-16 12:28:26 +0100

    creamos un programa para ingresar un numero y que se imprima en la pantalla

---
+++ ejercicio 1/generalidades.cpp
@@ -0,0 +1,13 @@
+Generalidades
+
+        Crea un nuevo proyecto en CLion y añade un archivo main.cpp. Escribe el siguiente código:
+
+#include <iostream>
+
+int main() {
+    int num;
+    std::cout << "Ingresa un número: ";
+    std::cin >> num;
+    std::cout << "El número ingresado es: " << num << std::endl;
+    return 0;
+}
